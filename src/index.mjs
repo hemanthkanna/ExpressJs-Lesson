8f4ff70a0dca00850548a472720beee8b192e50c
hemanthkanna <hemanthkanna20@gmail.com>
2024-03-18 11:28:59 +0530

    simple get request

--- src/index.mjs
+++ src/index.mjs
@@ -9,9 +9,49 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.status(200).send({
+  res.status(201).send({
     message: "Hello World!!",
   });
+});
+
+app.get("/api/users", (req, res) => {
+  res.status(201).send([
+    {
+      id: 1,
+      username: "kanna",
+      password: "<PASSWORD>",
+    },
+    {
+      id: 2,
+      username: "raj",
+      password: "<PASSWORD>",
+    },
+    {
+      id: 3,
+      username: "hemanth",
+      password: "<PASSWORD>",
+    },
+  ]);
+});
+
+app.get("/api/products", (req, res) => {
+  res.status(201).send([
+    {
+      id: 1,
+      name: "shirt",
+      price: 100,
+    },
+    {
+      id: 2,
+      name: "pants",
+      price: 200,
+    },
+    {
+      id: 3,
+      name: "shoes",
+      price: 300,
+    },
+  ]);
 });
 
 app.listen(PORT, () => {
