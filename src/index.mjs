838c023af7c7416e76376ee2eee68d8a54fa36cf
hemanthkanna <hemanthkanna20@gmail.com>
2024-03-18 11:54:57 +0530

    route params

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,42 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const sampleUsers = [
+  {
+    id: 1,
+    username: "kanna",
+    password: "<PASSWORD>",
+  },
+  {
+    id: 2,
+    username: "raj",
+    password: "<PASSWORD>",
+  },
+  {
+    id: 3,
+    username: "hemanth",
+    password: "<PASSWORD>",
+  },
+];
+
+const sampleUsersProducts = [
+  {
+    id: 1,
+    name: "shirt",
+    price: 100,
+  },
+  {
+    id: 2,
+    name: "pants",
+    price: 200,
+  },
+  {
+    id: 3,
+    name: "shoes",
+    price: 300,
+  },
+];
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -15,43 +51,27 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/users", (req, res) => {
-  res.status(201).send([
-    {
-      id: 1,
-      username: "kanna",
-      password: "<PASSWORD>",
-    },
-    {
-      id: 2,
-      username: "raj",
-      password: "<PASSWORD>",
-    },
-    {
-      id: 3,
-      username: "hemanth",
-      password: "<PASSWORD>",
-    },
-  ]);
+  res.status(201).send(sampleUsers);
 });
 
 app.get("/api/products", (req, res) => {
-  res.status(201).send([
-    {
-      id: 1,
-      name: "shirt",
-      price: 100,
-    },
-    {
-      id: 2,
-      name: "pants",
-      price: 200,
-    },
-    {
-      id: 3,
-      name: "shoes",
-      price: 300,
-    },
-  ]);
+  res.status(201).send(sampleUsersProducts);
+});
+
+app.get("/api/users/:id", (req, res) => {
+  console.log(req.params);
+  const parsedId = parseInt(req.params.id);
+  if (isNaN(parsedId)) {
+    return res.status(400).send({ message: "Bad Request. Invalid id" });
+  }
+
+  const user = sampleUsers.find((user) => user.id === parsedId);
+
+  if (!user) {
+    return res.status(404).send({ message: "User Not Found" });
+  }
+
+  return res.send(user);
 });
 
 app.listen(PORT, () => {
